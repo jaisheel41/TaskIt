ce0b7f49b70b89b0dee88e3c1f474411b87f758f
cakimpei <64963958+cakimpei@users.noreply.github.com>
2024-02-15 20:05:41 +0000

    Add context dict

--- Task/views.py
+++ Task/views.py
@@ -3,4 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 
 def homepage(request):
-    return render(request, 'homepage.html')
+    context_dict = {}
+    context_dict['username'] = 'User'
+    return render(request, 'homepage.html', context=context_dict)
